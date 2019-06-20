9034d7cde970a6fa271dbe4d6f4e9a78c39a0919
InfernoPlus <dmtinfernocide@gmail.com>
2019-06-19 19:07:39 -0500

    Anti cheat stuff

--- royale-server/game/src/main/java/org/infpls/royale/server/game/util/VirginSlayer.java
+++ royale-server/game/src/main/java/org/infpls/royale/server/game/util/VirginSlayer.java
@@ -10,6 +10,49 @@ public class VirginSlayer {
   
   private static final List<Virgin> VIRGINS = new ArrayList();
   
+  /* TODO: I should write this to a file but that would be more work so whatever.*/
+  public static final String[] BAN_LIST = new String[] {
+    "82.21.54.197",
+    "88.90.251.221",
+    "2.86.213.204",
+    "72.179.247.80",
+    "72.179.247.80",
+    "104.131.176.234",
+    "85.72.94.74",
+    "104.236.53.155",
+    "155.4.129.252",
+    "155.4.129.252",
+    "104.131.19.173",
+    "79.157.67.69",
+    "104.236.205.233",
+    "104.236.205.233",
+    "138.99.224.119",
+    "89.152.216.115",
+    "77.16.221.54",
+    "77.16.67.80",
+    "178.117.132.133",
+    "77.16.210.4",
+    "159.118.45.128",
+    "85.225.155.28",
+    "67.174.159.53",
+    "77.16.208.130",
+    "79.131.10.225",
+    "77.16.57.171",
+    "77.16.65.228",
+    "77.16.67.222",
+    "177.98.164.32",
+    "77.18.57.197",
+    "68.228.78.146",
+    "77.16.77.242",
+    "77.18.57.35",
+    "73.75.66.185",
+    "73.75.66.185",
+    "77.16.78.80",
+    "24.45.243.88",
+    "24.45.243.88",
+    "173.160.95.142",
+    "185.159.82.172"
+  };
   
   public static synchronized void strike(String ip) {
     try { 
@@ -40,6 +83,10 @@ public class VirginSlayer {
   public static boolean isBanned(String ip) {
     try { 
       final String ipt = ip.substring(1).split(":")[0].trim(); // Cut string to just IP
+      
+      for(int i=0;i<BAN_LIST.length;i++) {
+        if(BAN_LIST[i].equals(ipt)) { return true; }
+      }
       
       final Virgin v = get(ipt);
       if(v != null) { return v.strikes >= STRIKES; }
